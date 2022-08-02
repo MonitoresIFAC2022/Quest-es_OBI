a7486f1d29540e501f43891cf22818efecdd561e
AlonsoMCDG <110140005+AlonsoMCDG@users.noreply.github.com>
2022-08-02 15:54:30 -0500

    Update and rename Tarefas Programação Nível 2/2020/Fase 2/quebra.c to Tarefas Programação Nível 2/2020/Fase 2/Quebra-cabeças/quebra.c

--- Tarefas Programação Nível 2/2020/Fase 2/Quebra-cabeças/quebra.c
+++ Tarefas Programação Nível 2/2020/Fase 2/Quebra-cabeças/quebra.c
@@ -2,20 +2,22 @@
  * Alonso Martins
  * Quebra-cabeças
  * OBI2020 - Fase 2
+ * (Obs: Incompleto)
  */
 #include <stdio.h>
- 
-int main() {
 
-    int n, m;
-    scanf("%d", &n);
-    int tab[2][n];
-    for(int i = 0; i < 2; i ++) {
+int main()
+{
+    int N, m;
+    scanf("%d", &N);
+    int tab[2][N];
+    for (int i = 0; i < 2; i++)
+    {
         scanf("%d", &m);
-        for (int j = 0; j < m; j ++) {
+        for (int j = 0; j < m; j++)
+        {
             scanf("%d", &tab[i][j]);
         }
     }
-
     return 0;
 }
