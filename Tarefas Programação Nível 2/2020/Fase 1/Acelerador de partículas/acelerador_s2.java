3637926e437728ae4f48f8dbabb5e4bbc5440a77
AlonsoMCDG <110140005+AlonsoMCDG@users.noreply.github.com>
2022-08-02 11:13:20 -0500

    Add files via upload

---
+++ Tarefas Programação Nível 2/2020/Fase 1/Acelerador de partículas/acelerador_s2.java
@@ -0,0 +1,21 @@
+
+/*
+ * Alonso M.
+ * Acelerador de partículos
+ * OBI2020 - fase 1
+ */
+import java.util.*;
+
+public class acelerador_s2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int d = (in.nextInt() - 3) % 8;
+
+        System.out.println(d == 3 ? 1 : d == 4 ? 2 : 3);
+
+        in.close();
+    }
+}
