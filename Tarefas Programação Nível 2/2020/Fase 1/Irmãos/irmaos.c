5e63849bbddd48c2b41d83b0a847339baceed8ba
AlonsoMCDG <110140005+AlonsoMCDG@users.noreply.github.com>
2022-08-02 12:27:44 -0500

    Update and rename Tarefas Programação Nível 2/2020/Fase 1/irmaos.c to Tarefas Programação Nível 2/2020/Fase 1/Irmãos/irmaos.c

--- Tarefas Programação Nível 2/2020/Fase 1/Irmãos/irmaos.c
+++ Tarefas Programação Nível 2/2020/Fase 1/Irmãos/irmaos.c
@@ -1,14 +1,13 @@
 /*
  * Alonso Martins
  * Irmãos
- * OBI2020 - fase 1
+ * OBI2020 - Fase 1
 */
-
 #include <stdio.h>
 
 int main()
 {
-    int oscar, otavio;
+    int oscar, otavio; // idades de Oscar e Otávio
     scanf("%d %d", &oscar, &otavio);
     printf("%d\n", otavio + (otavio - oscar));
     return 0;
